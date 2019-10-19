d6385cb929857bbd5f8ba4eabf5ad8c79c740057
Aditya_Jayanti <33398626+Aditya9517@users.noreply.github.com>
2019-10-19 19:26:20 -0400

    update1-Selenium
    - Retrieves reviews for only one movie

--- reviews_analysis.py
+++ reviews_analysis.py
@@ -1,18 +1,43 @@
+import time
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 if __name__ == '__main__':
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    imdb_data = {}
+    url = "https://www.imdb.com/title/tt0094118/reviews?ref_=tt_ov_rt"
+    browser.get(url)
+    while True:
+        try:
+            button = browser.find_element_by_xpath('//*[@id="load-more-trigger"]')
+            button.click()
+            time.sleep(5)
+        except Exception as e:
+            break
 
-    with open('movie_urls.txt', 'r') as f:
-        urls = f.readlines()
+    page_source = browser.page_source
+    soup = BeautifulSoup(page_source, "html.parser")
 
-    url = urlopen('http://www.imdb.com/title/tt0111161/reviews?ref_=tt_ov_rt').read()
-    soup = BeautifulSoup(url, "html.parser")
+    movie_name = ""
+    for item in soup.find_all(class_="subpage_title_block"):
+        movie_name = str(item.find(class_="parent").text).rstrip('\r\n').lstrip().replace("\n", "").replace(" ", "")
+        imdb_data[movie_name] = []
+
+    title_review = {}
     for item in soup.find_all(class_="review-container"):
         review_title = item.find(class_="title").text
         review = item.find(class_="text").text
-        try:
-            rating = item.find(class_="point-scale").previous_sibling.text
-        except Exception as e:
-            rating = ""
-        print("Title: {}\nReview: {}\nRating: {}\n".format(review_title, review, rating))
+        # print("Title: {}\nReview: {}\n".format(review_title, review))
+        title_review['title'] = review_title
+        title_review['review'] = review
+        imdb_data[movie_name].append(title_review)
+
+    print(imdb_data)
+
+
+
+
+
