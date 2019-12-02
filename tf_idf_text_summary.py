76ed5fff0535d90a75e548aea80062ef98d2110b
Aditya9517 <aj8582@rit.edu>
2019-12-02 17:52:24 -0500

    added reading from json file

--- tf_idf_text_summary.py
+++ tf_idf_text_summary.py
@@ -1,18 +1,26 @@
+import json
 import math
 import re
 import textwrap
-import json
 
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize, sent_tokenize
 
-# threshold value
-# threshold value is directly proportional to the length of the summary
+# predefined max length of a sentence
+S = 10
+
+# threshold value is inversly proportional to the length of the summary
 THRESHOLD_VALUE = 0.8
 
 # text wrapper to wrap lines and indentation to each line
 wrapper = textwrap.TextWrapper(initial_indent='\t', subsequent_indent='\t', width=100)
+
+# reducing words to their root form using lemmatization
+word_lemmatizer = WordNetLemmatizer()
+
+# initializing nltk defined set of stop words for english
+stop_words = set(stopwords.words("english"))
 
 
 def eliminate_url_emoji(string):
@@ -39,12 +47,6 @@ def eliminate_stopwords_stemming(text):
     :param text: a collection of reviews
     :return: a word frequency matrix
     """
-    # initializing nltk defined set of stop words for english
-    stop_words = set(stopwords.words("english"))
-
-    # reducing words to their root form using stemming
-    root = PorterStemmer()
-
     # creating dictionary for the word frequency table
     word_frequency = {}
 
@@ -52,17 +54,19 @@ def eliminate_stopwords_stemming(text):
         freq = {}
         # splitting strings based on space and punctuation
         words = word_tokenize(sentence)
+        # eliminating stop words from consideration
+        words = [w for w in words if w not in stop_words]
         for word in words:
-            # reducing words to their root form using stemming
-            word = root.stem(word)
-            # removing stop words from consideration
-            if word in stop_words:
-                continue
+            # reducing words to their root form using lemmatization
+            word = word_lemmatizer.lemmatize(word)
+            # increase counter if it appears more than once
             if word in freq:
                 freq[word] += 1
+            # if word not a key in dictionary
             else:
                 freq[word] = 1
-        word_frequency[sentence[:10]] = freq
+        # considering sentence lengths up to 10
+        word_frequency[sentence[:S]] = freq
     return word_frequency
 
 
@@ -85,9 +89,13 @@ def summary_of_reviews(tf_idf_matrix, reviews, res_file):
     for sentence, frequency in tf_idf_matrix.items():
         # initialize total score for the sentence to 0
         total_score = 0
+        # number of items in nested dictionary
+        number_of_items = len(frequency)
         for word, score in frequency.items():
+            # score represents tf-idf value
             total_score += score
-        scores[sentence] = total_score/len(frequency)
+        scores[sentence] = total_score / number_of_items
+
     res_file.write("Sentence scores are:\n\n")
     # computing average scores
     sum_scores = sum(scores.values())
@@ -96,85 +104,106 @@ def summary_of_reviews(tf_idf_matrix, reviews, res_file):
     res_file.write('Average Score = ' + wrapper.fill(str(average_score)) + '\n\n')
 
     # generating a summary using a threshold value
+    threshold_value = average_score * THRESHOLD_VALUE
     # based on testing, it was found that a higher threshold value generates a shorter summary
     res_file.write("Generating summary for threshold value: \t" + str(THRESHOLD_VALUE) + "\n\n")
     for review in reviews:
-        if review[:10] in scores and scores[review[:10]] >= THRESHOLD_VALUE * average_score:
-            summary += " " + review
+        if review[:S] in scores and scores[review[:S]] >= threshold_value:
+            summary += review
     res_file.write("Summary generated is:\n\n")
     res_file.write(wrapper.fill(summary) + '\n')
     return summary
 
 
-if __name__ == '__main__':
+def text_summarization_using_tf_idf(all_text=None):
     with open('tf_idf_results.txt', 'w') as results_file:
         results_file.write("TF-IDF results file\n\n")
         with open('movie.txt', 'r') as test:
             all_reviews = test.readline()
 
-        # data cleaning process
-        all_reviews = eliminate_url_emoji(all_reviews)
-        # convert sentences in to tokens
-        sentences = sent_tokenize(all_reviews)
-        # creating a dictionary for the word frequency table
-        freq_of_words = eliminate_stopwords_stemming(sentences)
+            # data cleaning process
+            all_reviews = eliminate_url_emoji(all_reviews)
+            # convert sentences in to tokens
+            sentences = sent_tokenize(all_reviews)
+            # creating a dictionary for the word frequency table
+            freq_of_words = eliminate_stopwords_stemming(sentences)
 
-        # computing term frequency
-        # term frequency (tf) = (no of times term appears in a document/total number of terms in document)
-        # initializing term frequency matrix
-        term_frequency = {}
-        results_file.write("Computing Term Frequency\n\n")
-        for sentence, frequency in freq_of_words.items():
-            tf = {}
-            total_terms_in_document = len(frequency)
-            for word, terms_in_document in frequency.items():
-                tf[word] = terms_in_document / total_terms_in_document
-            term_frequency[sentence] = tf
+            # computing term frequency
+            # term frequency (tf) = (no of times term appears in a document/total number of terms in document)
+            # initializing term frequency matrix
+            term_frequency = {}
+            results_file.write("Computing Term Frequency\n\n")
+            for sentence, frequency in freq_of_words.items():
+                tf = {}
+                total_terms_in_document = len(frequency)
+                for word, terms_in_document in frequency.items():
+                    tf[word] = terms_in_document / total_terms_in_document
+                term_frequency[sentence] = tf
 
-        results_file.write(wrapper.fill(json.dumps(term_frequency)) + "\n\n")
+            results_file.write(wrapper.fill(json.dumps(term_frequency)) + "\n\n")
 
-        # builds a dictionary which describes the frequency of occurrence of a word in a text or document
-        number_of_documents_containing = {}
-        for sentence, f_matrix in freq_of_words.items():
-            for word, count in f_matrix.items():
-                if word in number_of_documents_containing:
-                    # a dictionary containing {word:frequency_of_occurrence_in_a_document}
-                    number_of_documents_containing[word] += 1
-                else:
-                    # initialize frequency count to 1, if word does not exist in dictionary
-                    number_of_documents_containing[word] = 1
+            # builds a dictionary which describes the frequency of occurrence of a word in a text or document
+            number_of_documents_containing = {}
+            for sentence, f_matrix in freq_of_words.items():
+                for word, count in f_matrix.items():
+                    if word in number_of_documents_containing:
+                        # a dictionary containing {word:frequency_of_occurrence_in_a_document}
+                        number_of_documents_containing[word] += 1
+                    else:
+                        # initialize frequency count to 1, if word does not exist in dictionary
+                        number_of_documents_containing[word] = 1
 
-        # computing inverse document frequency
-        # inverse document frequency (idf) = log(total no of documents/number of documents containing the term)
-        # initializing inverse document frequency
-        results_file.write("Computing inverse document frequency\n\n")
-        inverse_document_frequency = {}
-        # a word frequency matrix
-        for sentence, freq_matrix in freq_of_words.items():
-            inverse_doc_freq = {}
-            for word in freq_matrix.keys():
-                inverse_doc_freq[word] = math.log(len(sentences) / float(number_of_documents_containing[word]), 10)
-            inverse_document_frequency[sentence] = inverse_doc_freq
+            # computing inverse document frequency
+            # inverse document frequency (idf) = log(total no of documents/number of documents containing the term)
+            # initializing inverse document frequency
+            results_file.write("Computing inverse document frequency\n\n")
+            inverse_document_frequency = {}
+            # a word frequency matrix
+            for sentence, freq_matrix in freq_of_words.items():
+                inverse_doc_freq = {}
+                for word in freq_matrix.keys():
+                    inverse_doc_freq[word] = math.log(len(sentences) / float(number_of_documents_containing[word]),
+                                                      10)
+                inverse_document_frequency[sentence] = inverse_doc_freq
 
-        results_file.write(wrapper.fill(json.dumps(inverse_document_frequency)) + "\n\n")
+            results_file.write(wrapper.fill(json.dumps(inverse_document_frequency)) + "\n\n")
 
-        # computing tf-idf matrix
-        # function to compute tf-idf, tf-idf describes how important a word is in a collection of documents
-        # high f-idf value is proportional to the number of times a word appears in a document.
-        term_freq_inverse_doc_freq_matrix = {}
-        results_file.write("Computing term frequency * inverse document frequency\n\n")
-        for (s1, f1), (s2, f2) in zip(term_frequency.items(), inverse_document_frequency.items()):
-            tf_idf = {}
-            for (w1, v1), (w2, v2) in zip(f1.items(), f2.items()):
-                tf_idf[w1] = float(v1 * v2)
-            term_freq_inverse_doc_freq_matrix[s1] = tf_idf
+            # computing tf-idf matrix
+            # function to compute tf-idf, tf-idf describes how important a word is in a collection of documents
+            # high f-idf value is proportional to the number of times a word appears in a document.
+            term_freq_inverse_doc_freq_matrix = {}
+            results_file.write("Computing term frequency * inverse document frequency\n\n")
+            for (x1, y1), (x2, y2) in zip(term_frequency.items(), inverse_document_frequency.items()):
+                tf_idf = {}
+                for (x3, y3), (x4, y4) in zip(y1.items(), y2.items()):
+                    tf_idf[x3] = float(y3) * float(y4)
+                term_freq_inverse_doc_freq_matrix[x1] = tf_idf
+                # print(term_freq_inverse_doc_freq_matrix)
 
-        results_file.write(wrapper.fill(json.dumps(term_freq_inverse_doc_freq_matrix)) + "\n\n")
+            results_file.write(wrapper.fill(json.dumps(term_freq_inverse_doc_freq_matrix)) + "\n\n")
 
-        # original text
-        results_file.write("Original file content" + "\n\n")
-        results_file.write(wrapper.fill(all_reviews) + "\n\n")
-        # generate summary
-        summary_of_movie_reviews = summary_of_reviews(term_freq_inverse_doc_freq_matrix, sentences, results_file)
-        print(summary_of_movie_reviews)
+            # original text
+            results_file.write("Original file content" + "\n\n")
+            results_file.write(wrapper.fill(all_reviews) + "\n\n")
+            # generate summary
+            summary_of_movie_reviews = summary_of_reviews(term_freq_inverse_doc_freq_matrix, sentences,
+                                                          results_file)
+            print(summary_of_movie_reviews)
 
+
+if __name__ == '__main__':
+    read_from_file = False
+
+    if read_from_file:
+        # reads from json file
+        with open('movie_sampled_data.json', 'r') as f:
+            movie_file = json.load(f)
+        # for each movie perform text summarization on all the reviews
+        for movie_name in movie_file:
+            text = ""
+            for j in movie_file[movie_name]:
+                text += eliminate_url_emoji(j['review'])
+            text_summarization_using_tf_idf(text)
+            break
+    else:
+        text_summarization_using_tf_idf()
